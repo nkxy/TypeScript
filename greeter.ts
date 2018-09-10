687bc727b5bf36c65695c53fa5d1b736d167cf8d
Nick Yang <nick.yang@rangle.io>
2018-09-10 11:10:53 -0400

    Added Student class

--- greeter.ts
+++ greeter.ts
@@ -1,13 +1,19 @@
+class Student {
+    fullName: string;
+    constructor(public firstName: string, public lastName: string) {
+        this.fullName = firstName + " " + lastName;
+    }
+}
+
 interface Person {
     firstName: string;
     lastName: string;
-    age: number;
 }
 
 function greeter(name: Person) {
     return "Hello " + name.firstName + name.lastName;
 }
 
-let user = {firstName: "Nick", lastName: "Yang", age: 18};
+let user = new Student("Nick", "Yang");
 
 document.body.innerHTML = greeter(user); 
