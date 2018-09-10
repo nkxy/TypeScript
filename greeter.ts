86dff0553eaca8562756122c95eff10b45806f22
Nick Yang <nick.yang@rangle.io>
2018-09-10 10:58:30 -0400

    Added interface to Greeter

--- greeter.ts
+++ greeter.ts
@@ -1,7 +1,13 @@
-function greeter(name) {
-    return "Hello " + name;
+interface Person {
+    firstName: string;
+    lastName: string;
+    age: number;
 }
 
-let user = "Nick Yang";
+function greeter(name: Person) {
+    return "Hello " + name.firstName + name.lastName;
+}
+
+let user = {firstName: "Nick", lastName: "Yang", age: 18};
 
 document.body.innerHTML = greeter(user); 
